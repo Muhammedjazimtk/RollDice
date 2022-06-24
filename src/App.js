484c59c000bc50ecde6b3a8644ae5c4ca2a06875
Muhammedjazimtk <77974989+Muhammedjazimtk@users.noreply.github.com>
2022-06-25 00:08:51 +0530

    added js functionality

--- src/App.js
+++ src/App.js
@@ -6,12 +6,33 @@ function App() {
 
 
   const [name,setName] = useState('Click the  Button to roll the Dice🎲!!')
-  // useEffect(()=>{
+  const [img1 , setImg1] = useState('images/dice6.png')
+  const [img2 , setImg2] = useState('images/dice6.png')
 
-  // },[])
 
-  var r1 = Math.floor(Math.random()*6)
-  var r2 = Math.floor(Math.random()*6)
+
+  
+
+  const handleClick = ()=>{
+    var r1 = Math.floor(Math.random()*6 + 1)
+    var r2 = Math.floor(Math.random()*6 + 1)
+    
+    setImg1('images/dice'+r1+'.png')
+    setImg2('images/dice'+r2+'.png')
+
+    console.log(img1 , img2);
+
+    if(r1>r2){
+      setName('Player 1 wins 🥇')
+    }else if(r1<r2){
+
+      setName('Player 2 wins 🥇')
+    }else{
+      setName('Draw !!')
+    }
+  }
+
+  
 
   // console.log(r1,r2);
 
@@ -19,17 +40,13 @@ function App() {
     <div className='flex  flex-col justify-center bg-[#2D4263] min-h-screen w-full'>
       <div className='flex flex-col items-center poppins space-y-5 '>
         <h1 className='text-[#C84B31] text-5xl font-[200] py-10'>{name} </h1>
-        <div className='flex flex-col justify-center bg-[#ECDBBA] w-[130px] h-[40px] rounded-md' onClick={()=>{
-          setName(()=>{
-            return 'Clicked'
-          })
-          }}>
+        <div className='flex flex-col justify-center bg-[#ECDBBA] w-[130px] h-[40px] rounded-md' onClick={handleClick}>
          <p className='flex flex-col items-center font-[600]'>Roll</p>
         </div>
 
         <div className='flex justify-between p-10 space-x-3'>
-        <img src='images/dice6.png' className='px-4' alt='dice img'/>        
-        <img src='images/dice6.png' className='px-4' alt='dice img'/>        
+        <img src={img1} className='px-4' alt='dice img'/>        
+        <img src={img2} className='px-4' alt='dice img'/>        
         
         </div>
       
